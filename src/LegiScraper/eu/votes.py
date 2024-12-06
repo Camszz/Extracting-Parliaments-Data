9966e710bfec4d9759aa51a96f66bcfcc6367edc
KillianVar <varesconkillian@gmail.com>
2024-12-06 15:51:08 +0100

    added votes of MPs in EU.

--- src/LegiScraper/eu/votes.py
+++ src/LegiScraper/eu/votes.py
@@ -25,7 +25,9 @@ class Votes:
         keywords, topics = self.topic_analysis(votes_to_extract.drop_duplicates(subset='display_title'))
         topic_res = pd.concat([keywords, topics], axis=1).set_index('sequence')
         votes = votes_to_extract.set_index('display_title').join(topic_res, validate='m:1').reset_index()
+        df_mp_votes = self.mp_votes(votes)
         save_dataframe_to_folder(votes, folder_path=self.scraper.config['output_folder'], file_name='votes_eu.csv')
+        save_dataframe_to_folder(df_mp_votes, folder_path=self.scraper.config['output_folder'], file_name='member_votes_eu.csv')
 
     def extract_votes(self,):
         df = pd.json_normalize(self.scraper.get_data()['results'])
@@ -38,3 +40,20 @@ class Votes:
         topics = self.topic_analyzer.topic_classifier(votes['display_title'])
 
         return keywords, topics
+    
+    def mp_votes(self, votes_to_extract):
+
+        df = []
+
+        for id in tqdm(votes_to_extract['id']):
+            data_request = f"/{id}"
+            vote = self.scraper.get_data(data_request=data_request)
+            df_membervotes_id = pd.json_normalize(vote['member_votes'])[['member.id','position']]
+            df_membervotes_id.loc[:, 'vote-id'] = id
+            df.append(df_membervotes_id)
+            sleep(0.5)
+        
+        df = pd.concat(df)
+        df.rename(columns={'member.id' : 'member-id'}, inplace=True)
+        
+        return df
